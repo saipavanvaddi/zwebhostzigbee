76566cb03ebd0d3ea82c4290a8518a3fc8c2a03c
saipavanvaddi <saipavan2930@gmail.com>
2024-01-27 20:37:31 +0530

    index.html

---
+++ db_connect.php
@@ -0,0 +1,65 @@
+<?php
+
+class DB_CONNECT {
+    public $con;
+ 
+    // Constructor
+    function __construct() {
+        // Trying to connect to the database
+        $this->connect();
+    }
+ 
+    // Destructor
+    function __destruct() {
+        // Closing the connection to database
+        $this->close();
+    }
+ 
+   // Function to connect to the database
+    function connect() {
+
+        //importing dbconfig.php file which contains database credentials 
+        $filepath = realpath (dirname(__FILE__));
+
+        require_once($filepath."/dbconfig.php");
+        
+		// Connecting to mysql (phpmyadmin) database
+        $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysqli_error());
+ 
+        // Selecing database
+        $db = mysqli_select_db($con,DB_DATABASE) or die(mysqli_error()) or die(mysqli_error());
+ 
+        // returing connection cursor
+        return $con;
+    }
+      public function getConnection() {
+         //importing dbconfig.php file which contains database credentials 
+        $filepath = realpath (dirname(__FILE__));
+
+        require_once($filepath."/dbconfig.php");
+        
+		// Connecting to mysql (phpmyadmin) database
+        $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysqli_error());
+ 
+        // Selecing database
+        $db = mysqli_select_db($con,DB_DATABASE) or die(mysqli_error()) or die(mysqli_error());
+ 
+        // returing connection cursor
+        return $con;
+    }
+ 
+	// Function to close the database
+    function close() {
+        // Closing data base connection
+        $filepath = realpath (dirname(__FILE__));
+
+        require_once($filepath."/dbconfig.php");
+        
+		// Connecting to mysql (phpmyadmin) database
+        $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysqli_error());
+        mysqli_close($con);
+    }
+ 
+}
+ 
+?>
---
+++ dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+/****************************************************/
+/************** Created by : Vivek Gupta ************/
+/***************     www.vsgupta.in     *************/
+/***************     www.iotmonk.com     *************/
+/****************************************************/ 
+
+define('DB_USER', "id21799360_saiiot");     // Your database user name
+define('DB_PASSWORD', "Web@1234");			// Your database password 
+define('DB_DATABASE', "id21799360_saiiot"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will 'local' host)
+
+?>
